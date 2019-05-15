dbc98aedbebe6fe5f3fea340c4e2d0ed052df6d3
orev1897 <50280737+orev1897@users.noreply.github.com>
2019-05-15 23:10:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -83,7 +83,7 @@
                 }
                 echo "</table>";
             } else {
-                echo "<h3>No one is currently registered.</h3>";
+                echo "<h3>Belum ada pesan yang masuk</h3>";
             }
         } catch(Exception $e) {
             echo "Failed: " . $e;
